13ed5d5cd6338c1c73f1470746573f56acda7abf
CanadianCrafter <bryan335667879@gmail.com>
2020-11-15 10:25:05 -0500

    CCC 09

---
+++ CCC/CCC 09/CCC09J1ISBN.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class CCC09J1ISBN {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int num = 91+ readInt()+3*readInt()+readInt();
+    	System.out.printf("The 1-3-sum is %d",num);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
