2fcca45b0b008ba5aef836d5be048c38ff51b1df
CanadianCrafter <bryan335667879@gmail.com>
2020-12-18 12:15:54 -0500

    DMOPC

---
+++ DMOPC/DMOPC20C2P2LousyChristmasPresents.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+public class DMOPC20C2P2LousyChristmasPresents {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int N = readInt();
+    	int M = readInt();
+    	int a[] = new int [M];
+    	int b[] = new int [M];
+    	int c[] = new int [N];
+    	int lowest[] = new int [1000005];
+    	Arrays.fill(lowest, 10000000);
+    	int highest[] = new int [1000005];
+    	Arrays.fill(highest, -1);
+    	for(int i =0;i<N;i++) {
+    		int x = readInt();
+    		lowest[x]=Math.min(lowest[x], i);
+    		highest[x]=Math.max(highest[x], i);
+    	}
+    		
+    	for(int i =0;i<M;i++) {
+    		a[i]=readInt();
+    		b[i]=readInt();
+    	}
+    	int max = 0;
+    	for(int i =0;i<M;i++) {
+    		max = Math.max(max, highest[b[i]]-lowest[a[i]]+1);
+    	}
+    	System.out.println(max);
+    	
+    	
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
