e6affd79ea97c3795d23d67f010215ba81ad2e7d
CanadianCrafter <bryan335667879@gmail.com>
2021-01-14 19:33:59 -0500

    DMOPC

---
+++ DMOPC/DMOPC17C3P02Satisfiability.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class DMOPC17C3P02Satisfiability {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	double num = readDouble()/2;
+    	System.out.printf("$%.2f $%.2f",num,num);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
