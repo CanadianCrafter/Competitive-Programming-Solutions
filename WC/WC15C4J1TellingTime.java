c2cce87f9aa251b7dd322fad5dc2626e528765c7
CanadianCrafter <bryan335667879@gmail.com>
2021-01-18 23:40:47 -0500

    WC

---
+++ WC/WC15C4J1TellingTime.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class WC15C4J1TellingTime {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int N = readInt();
+    	int G = readInt();
+    	int ans =0;
+    	for(int i =0;i<N;i++) {
+    		if(readInt()%G==0)ans++;
+    	}
+    	System.out.println(ans);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
---
+++ WC/WC16C1J1ASpookySeason.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class WC16C1J1ASpookySeason {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int S = readInt();
+    	String str="sp";
+    	for(int i =0;i<S;i++)str+="o";
+    	str+="ky";
+    	System.out.println(str);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
