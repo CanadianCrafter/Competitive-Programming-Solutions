cc0629c306fb82d5b9810c67992fefe175997c20
CanadianCrafter <bryan335667879@gmail.com>
2021-01-25 14:17:05 -0500

    WC

---
+++ WC/WC17C3J1LikeCommentAndSubscribe.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class WC17C3J1LikeCommentAndSubscribe {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int S = readInt();
+    	double n = Math.log(S)/Math.log(10);
+    	if(n%1==0)n++;
+    	n=Math.ceil(n);
+    	int ans= (int) Math.pow(10, n)-S;
+    	System.out.println(ans);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
---
+++ WC/WC17C4J1FightOrFlight.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class WC17C4J1FightOrFlight {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	String arr[] = new String[2];
+    	for(int i =0;i<2;i++)
+    		arr[i]=next();
+    	if(arr[0].equals(arr[1]))System.out.println(arr[0]);
+    	else System.out.println("Undecided");
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
