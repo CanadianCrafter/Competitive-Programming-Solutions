5926c231b3fffe6db7ba6833b849a3cc3f849a72
CanadianCrafter <bryan335667879@gmail.com>
2021-01-27 12:14:21 -0500

    WC

---
+++ WC/WC18C2J1AlternateAccess.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class WC18C2J1AlternateAccess {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int A = readInt();
+    	int B = readInt();
+    	int U = readInt();
+    	int D = readInt();
+    	System.out.println(A>B?D*(A-B):U*(B-A));
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
---
+++ WC/WC18C2J2ThisMessageWillSelfDestruct.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+public class WC18C2J2ThisMessageWillSelfDestruct {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	String str[] = readLine().split(":");
+    	System.out.println(Integer.parseInt(str[0])*60+Integer.parseInt(str[1]));
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
