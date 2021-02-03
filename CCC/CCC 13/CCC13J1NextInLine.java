f01d85ad747faaa3b6ecc106cdb8505a5b1b6a7b
CanadianCrafter <bryan335667879@gmail.com>
2021-02-03 08:22:54 -0500

    CCC 13

---
+++ CCC/CCC 13/CCC13J1NextInLine.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class CCC13J1NextInLine {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int Y = readInt();
+    	int M = readInt();
+    	System.out.println(2*M-Y);
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
