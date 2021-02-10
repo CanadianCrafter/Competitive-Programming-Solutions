303273fed4d02adf11c2d131e21a6d83fdb3aabd
CanadianCrafter <bryan335667879@gmail.com>
2021-02-10 11:11:45 -0500

    MWC

---
+++ MWC/MWC15C1P1PlaylistPanic.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+public class MWC15C1P1PlaylistPanic {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int N = readInt();
+    	int a[] = new int [N];
+    	for(int i =0;i<N;i++) {
+    		a[i]=60*readInt()+readInt();
+    	}
+    	Arrays.sort(a);;
+    	int t = readInt()*60+readInt();
+    	int ans=0;
+    	int time=0;
+    	for(int i =0;i<N;i++) {
+    		if(a[i]+time>t) {
+    			System.out.println(ans);
+    			return;
+    		}
+    		time+=a[i];
+    		ans++;
+    	}
+    	System.out.println(ans);
+    	
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
