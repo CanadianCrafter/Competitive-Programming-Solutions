f7e99ee6e9570ad06d50dab19a148eb997c73ec5
CanadianCrafter <bryan335667879@gmail.com>
2021-04-20 13:11:18 -0400

    WC C++

---
+++ WC/WC15C2J1ANewHope.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    int num=0;
+    cin >> num;
+    cout << "A long time ago in a galaxy";
+    for(int i = 0; i < num-1; i++) cout << " far,";
+    cout << " far away..." <<endl;
+}
