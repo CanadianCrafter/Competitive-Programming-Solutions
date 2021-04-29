62df5c01f5afac52dfa075d182690e63169d38a0
CanadianCrafter <bryan335667879@gmail.com>
2021-04-29 12:26:18 -0400

    WC C++

---
+++ WC/WC16C1J1ASpookySeason.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    int N;
+    cin>>N;
+    cout<<"sp";
+    for(int i = 0; i<N;i++){
+        cout << "o";
+    }
+    cout << "ky";
+}
