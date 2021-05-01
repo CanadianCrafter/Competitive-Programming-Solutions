f8a9ead88395241eacb8218633eb5841f946aba3
CanadianCrafter <bryan335667879@gmail.com>
2021-04-30 23:19:08 -0400

    CCC 04 C++

---
+++ CCC/CCC 04/CCC04J1Squares.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <vector> 
+#include <stdio.h>
+#include <math.h> 
+
+using namespace std;
+int main(){
+    
+    int N;
+    cin >> N;
+    int ans= sqrt(N);
+    printf("The largest square has side length %d.",ans);
+        
+}
