971f8c94cb4a162216053f78044a813e0a903815
CanadianCrafter <bryan335667879@gmail.com>
2021-06-09 22:39:43 -0400

    Uncategorized

---
+++ Uncategorized/CatLovers.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class CatLovers {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+    	int N = readInt();
+    	int total=0;
+    	for(int i =0;i<N;i++) {
+    		if(next().equals("cats")) total++;
+    		else total--;
+    	}
+    	if(total==0) {
+    		System.out.println("equal");
+    		return;
+    	}
+    	System.out.println(total>0?"cats":"dogs");
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
